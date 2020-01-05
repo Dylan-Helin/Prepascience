6459659dddb9a0c3dba722802c70bd695ccada27
Dylan-Helin <Dylan-Helin@users.noreply.github.com>
2020-01-05 14:11:19 +0100

    mise en place de la base de données

--- comptes/admin.py
+++ comptes/admin.py
@@ -1,6 +1,17 @@
 from django.contrib import admin
+from .models import Personne
+from .models import Projet
+from .models import PersonneProjet
+from .models import ProjetMateriel
+from .models import Materiaux
+from .models import Admin
 
 # Register your models here.
 
-from .models import Personne
+
 admin.site.register(Personne)
+admin.site.register(ProjetMateriel)
+admin.site.register(Projet)
+admin.site.register(PersonneProjet)
+admin.site.register(Admin)
+admin.site.register(Materiaux)
