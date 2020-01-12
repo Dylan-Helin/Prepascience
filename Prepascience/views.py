2556876cd0d2263a4420dbda9c4c88fcb903c30d
Dylan-Helin <Dylan-Helin@users.noreply.github.com>
2020-01-12 16:49:31 +0100

    début de la page matériaux

--- Prepascience/views.py
+++ Prepascience/views.py
@@ -1,10 +1,16 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-from comptes.models import Personne
+from comptes.models import *
 
 
 def homepage(request):
     personnes = Personne.objects.all()
-    list=[1]*10
-    return render(request,"home.html", {'personnes': personnes,'list':list})
+    list = [1] * 10
+    return render(request, "home.html", {'personnes': personnes, 'list': list})
+
+
+def materiaux(request):
+    mat = Materiaux.objects.all()
+    j = 0
+    return render(request, "materiaux.html", {'mat': mat, 'j': j})
