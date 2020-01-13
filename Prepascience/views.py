cc2353669ff0b1a13b996021e416024a5d79652c
Angele-Guery <Angele-Guery@users.noreply.github.com>
2020-01-13 19:41:58 +0100

    commencement dela page profil

--- Prepascience/views.py
+++ Prepascience/views.py
@@ -5,12 +5,13 @@ from comptes.models import *
 
 
 def homepage(request):
-    personnes = Personne.objects.all()
-    list = [1] * 10
-    return render(request, "home.html", {'personnes': personnes, 'list': list})
+    return render(request, "home.html")
 
 
 def materiaux(request):
     mat = Materiaux.objects.all()
-    j = 0
-    return render(request, "materiaux.html", {'mat': mat, 'j': j})
+    return render(request, "materiaux.html", {'mat': mat})
+
+
+def profil(request):
+    return render(request, "profil.html")
