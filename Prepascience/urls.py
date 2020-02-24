0aad42817fe38b98053440f0bf0e3c9a976f7fe9
StevenHelin <StevenHelin@users.noreply.github.com>
2020-02-24 16:51:39 +0100

    Logout disponible (erreur lors du retour a l accueil)
    mise a jour de la nav bar pour profil dans toute les pages déja presentes

--- Prepascience/urls.py
+++ Prepascience/urls.py
@@ -32,6 +32,7 @@ urlpatterns = [
     path('ajout/',views.ajout),
     path('demandead/',views.demandead),
     path('ajoutProjet/', views.ajoutProjet.as_view()),
+    path('logout/',LogoutView.as_view(template_name='logout.html')),
 ]
 
 urlpatterns+=staticfiles_urlpatterns()
